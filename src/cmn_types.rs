7bcda5e0dd03702d760929d80822cc088ec2b5c6
Martin Törnqvist <m.tornq@gmail.com>
2016-02-25 21:51:56 +0100

    Can move a cursor with keyboard

--- src/cmn_types.rs
+++ src/cmn_types.rs
@@ -11,3 +11,37 @@ impl Default for P
         P { x: 0, y: 0 }
     }
 }
+
+pub fn p_sum(p1: &P, p2: &P) -> P
+{
+    P {
+        x: p1.x + p2.x,
+        y: p1.y + p2.y,
+    }
+}
+
+pub enum Dir
+{
+    Right,
+    Left,
+    Down,
+    Up,
+    DownRight,
+    UpRight,
+    DownLeft,
+    UpLeft,
+}
+
+pub fn to_offset(dir: Dir) -> P
+{
+    match dir {
+        Dir::Right => P { x: 1, y: 0 },
+        Dir::Left => P { x: -1, y: 0 },
+        Dir::Down => P { x: 0, y: 1 },
+        Dir::Up => P { x: 0, y: -1 },
+        Dir::DownRight => P { x: 1, y: 1 },
+        Dir::UpRight => P { x: 1, y: -1 },
+        Dir::DownLeft => P { x: -1, y: 1 },
+        Dir::UpLeft => P { x: -1, y: -1 },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-mod io;
-use io::*;
+#[cfg(unix)]
+mod ncurses_mode;
+#[cfg(unix)]
+use ncurses_mode::io;
 
 mod cmn_types;
 use cmn_types::*;
+
+mod cursor;
 
 fn main()
 {
@@ -10,29 +14,76 @@ fn main()
 
     io::init();
 
-    loop {
-        io::clear_scr();
+    let mut cursor_p = P { ..P::default() };
 
+    io::clear_scr();
+
+    // ------------------------------------------------------------------------
+    // Game loop
+    // ------------------------------------------------------------------------
+    loop {
         let scr_dim = io::scr_dim();
 
         for x in 0..scr_dim.x {
             for y in 0..scr_dim.y {
-                io::draw_char(P { x: x, y: y }, '.', CLR_GRN, CLR_BLK, FONT_NORM);
+                io::draw_char(&P { x: x, y: y },
+                              '.',
+                              io::CLR_GRN,
+                              io::CLR_BLK,
+                              io::FONT_NORM);
             }
         }
 
-        io::draw_text(P { x: 1, y: 1 },
+        io::draw_text(&P { x: 1, y: 1 },
                       &format!("Some number: {}", 42),
-                      CLR_WHI,
-                      CLR_BLK,
-                      FONT_BOLD);
+                      io::CLR_WHI,
+                      io::CLR_BLK,
+                      io::FONT_BOLD);
+
+        io::draw_char(&cursor_p, 'X', io::CLR_GRN, io::CLR_BLK, true);
 
         io::update_scr();
 
         let inp = io::get_input();
 
-        if inp == 'q' as i32 {
-            break;
+        match inp {
+            ('q', 0) => {
+                break;
+            }
+
+            ('6', 0) | ('\n', io::KEY_RIGHT) => {
+                cursor::offset(Dir::Right, &mut cursor_p);
+            }
+
+            ('4', 0) | ('\n', io::KEY_LEFT) => {
+                cursor::offset(Dir::Left, &mut cursor_p);
+            }
+
+            ('2', 0) | ('\n', io::KEY_DOWN) => {
+                cursor::offset(Dir::Down, &mut cursor_p);
+            }
+
+            ('8', 0) | ('\n', io::KEY_UP) => {
+                cursor::offset(Dir::Up, &mut cursor_p);
+            }
+
+            ('3', 0) => {
+                cursor::offset(Dir::DownRight, &mut cursor_p);
+            }
+
+            ('9', 0) => {
+                cursor::offset(Dir::UpRight, &mut cursor_p);
+            }
+
+            ('1', 0) => {
+                cursor::offset(Dir::DownLeft, &mut cursor_p);
+            }
+
+            ('7', 0) => {
+                cursor::offset(Dir::UpLeft, &mut cursor_p);
+            }
+
+            _ => {}
         }
     }
 
